9fd1fdd8b338b599178375fe3b9b4b2c4bfb2a87
Justin Lazarski <blackhawks189@gmail.com>
2020-10-07 02:04:58 -0700

    first commit

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Group 1 PHP Project.
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Group 1: My Grocery Store</title>
+    <link rel="stylesheet" type="text/css"
+          href="/cis4260/GroupProject/ch5GroupDB/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Grocery Store</h1></header>
