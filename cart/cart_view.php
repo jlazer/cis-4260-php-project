9dd4b7d1781ff88d121827a8d23200548e545776
Justin Lazarski <blackhawks189@gmail.com>
2020-10-08 13:45:31 -0700

    Updated cart_view.php
    Added a link to the home page from the cart_view.php file

--- cart/cart_view.php
+++ cart/cart_view.php
@@ -62,6 +62,7 @@
         <?php endif; ?>
         <p><a href=".?action=show_add_item">Add Item</a></p>
         <p><a href=".?action=empty_cart">Empty Cart</a></p>
+        <p><a href="../index.php">Home</a></p>
     </main>
 </body>
 </html>
